63e2c49a425197fa30f809c07632e95ca2ecd009
Fritz Anderson <fritza@manoverboard.org>
2014-06-02 11:12:52 -0500

    Party: destroy dependents (guests) upon deletion.

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,6 @@
 class Party < ActiveRecord::Base
-  has_many :guests
+  has_many :guests, dependent: :destroy
+  # Change :dependent to :nullify if Party:Guest ever becomes many-to-many
   
   def human_name
     '%s — %s' % [self.theme, self.when_held.strftime("%A, %B %-d")]
