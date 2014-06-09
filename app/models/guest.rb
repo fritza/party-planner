a4a52c3b76e216c94d07a2ae042101b18e980b91
Fritz Anderson <fritza@manoverboard.org>
2014-06-08 20:32:47 -0500

    Add validation to Guest, and a defect in Guest::new_from_cnetid.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,13 +2,15 @@ require 'ldap-utils'
 
 class Guest < ActiveRecord::Base
   belongs_to :party
+  validates_format_of :email, with: /\w+@\w+\.\w+/
+  validates_presence_of :name
   
   def self.new_from_cnetid(cnet_id)
     raise ArgumentError, "CNetID must be a string" unless cnet_id.kind_of? String
     
     record = LDAPUtils::ldap_record_for_cnetid(cnet_id)
     # Oh. Maybe this should not be commented out:
-    # return nil unless record
+    return nil unless record
     Guest.new name:   record[:displayname][0],
               email:  record[:mail][0]
   end
